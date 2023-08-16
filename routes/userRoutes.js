cc52924c96ccd3ee77a845ad16e1417856410b69
Binula Rasanjith <binula28@gmail.com>
2023-08-17 02:50:58 +0530

    min change

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,11 @@ import { Router } from "express";
 import { authUserRole } from "../middlewares/authUser.js";
 import { ADMIN } from "../constants/constants.js";
 import { addUser, getUsers } from "../controllers/userController.js";
-import upload from "../middlewares/imageUpload.js";
+import { profilePicUpload } from "../middlewares/imageUpload.js";
 
 const router = Router();
 
-router.post("/add-user", upload.single("image"), addUser);
+router.post("/add-user", profilePicUpload.single("image"), addUser);
 router.post("/get-users", getUsers);
 
 export default router;
