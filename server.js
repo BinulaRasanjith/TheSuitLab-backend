701e740b9fe04deb9a5905bab0c915fde468addf
Binula Rasanjith <binula28@gmail.com>
2023-08-17 02:50:05 +0530

    static folder added (uploads)

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express'; // for creating express app
 import dotenv from 'dotenv'; // for loading environment variables from .env file
 import cors from 'cors';  // for enabling CORS
 import cookieParser from 'cookie-parser'; // for parsing cookies
+import path from 'path'; // for working with file and directory paths
+import { fileURLToPath } from 'url'; // for getting file path from url
 
 import sequelize from './db/db.js'; // for connecting to database and creating tables
 import seed from './db/seed.js';
@@ -11,11 +13,14 @@ import { ASCII } from './config/config.js'; // for ASCII art
 
 dotenv.config(); // for loading environment variables from .env file
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = express(); // for creating express app
 
 // for parsing request body and cookies from request headers 
 app.use(express.json()); // for parsing application/json 
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded 
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cors({ // for enabling CORS
     origin: [ // for allowing requests from these origins
         process.env.FRONTEND_URL || 'http://localhost:3000',
@@ -23,15 +28,15 @@ app.use(cors({ // for enabling CORS
     ],
     credentials: true // for allowing cookies to be sent from frontend
 }));
-
-app.use(cookieParser()); // for parsing cookies from request headers 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // for serving static files
+app.use(cookieParser()); // for parsing cookies from request headers
 
 app.use('/api', routes); // for routing to different endpoints
 
 
 // connect to database and create tables if not exists and start server if success else exit process if error 
 sequelize
-    .sync({ alter: true, force: true }) // for creating tables if not exists and alter tables if exists and force: true for dropping tables if exists and creating new tables 
+    .sync({ alter: true, }) // for creating tables if not exists and alter tables if exists and force: true for dropping tables if exists and creating new tables 
     .then(() => {
         // if success, log and continue process
         console.log(`${ASCII.green}\nDatabase connection established!${ASCII.reset}`);
