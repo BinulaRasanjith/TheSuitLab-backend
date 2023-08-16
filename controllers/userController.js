bad7f72d715f7d3b774c463e2c2231ac4f5e833a
Binula Rasanjith <binula28@gmail.com>
2023-08-16 22:29:48 +0530

    add users method added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,5 @@
-import { fileURLToPath } from 'url';
 import { ACTIVE } from '../constants/constants.js';
 import User from '../models/UserModel.js';
-// import multer from 'multer';
-// import path from 'path';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// const storage = multer.diskStorage({
-//     destination: path.join(__dirname, '../uploads'), // Set the destination folder for uploads
-//     filename: (req, file, cb) => {
-//         // Generate a unique filename for the uploaded file
-//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-//         cb(null, uniqueSuffix + path.extname(file.originalname));
-//     }
-// });
-
-// const upload = multer({ storage: storage });
 
 export const addUser = async (req, res) => {
 
@@ -34,7 +17,6 @@ export const addUser = async (req, res) => {
         image
     } = req.body;
     const imagePath = req.file ? req.file.filename : null;
-    //console.log(image);
 
     try {
         const user = await User.create({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,13 +3,11 @@ import { Router } from "express";
 import { authUserRole } from "../middlewares/authUser.js";
 import { ADMIN } from "../constants/constants.js";
 import { addUser, getUsers } from "../controllers/userController.js";
+import upload from "../middlewares/imageUpload.js";
 
 const router = Router();
 
 router.post("/add-user", upload.single("image"), addUser);
 router.post("/get-users", getUsers);
-import { addUser } from "../controllers/userController.js";
-import upload from "../middlewares/imageUpload.js";
-// router.post("/add-user", addUser);
 
 export default router;
