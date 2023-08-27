9ff3b0bd4d8226cdb07a7ff8583c570eddc387e1
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-08-27 13:19:36 +0530

    twilio addition

---
+++ config/twilioConfig.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+module.exports = {
+    accountSid: 'your_account_sid',
+    authToken: 'your_auth_token',
+    phoneNumber: 'your_phone_number'
+};
