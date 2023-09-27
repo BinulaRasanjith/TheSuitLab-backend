878e64a515b4b61dea83ce188331998c369927a6
Binula Rasanjith <binula28@gmail.com>
2023-09-27 21:08:02 +0530

    get all purchase orders

--- controllers/purchaseOrderController.js
+++ controllers/purchaseOrderController.js
@@ -4,13 +4,21 @@ export const getPurchaseOrders = async (req, res) => {
 
     try {
         const purchaseOrders = await PurchaseOrder.findAll();
-        const purchaseOrdersWithCustomer = [];
-        purchaseOrders.forEach(async (purchaseOrder) => {
+
+        // purchaseOrders.forEach(async (purchaseOrder) => {
+        //     const user = await User.findOne({ where: { userId: purchaseOrder.customerId } });
+        //     purchaseOrder.customerName = `${user.firstName} ${user.lastName}`
+
+        // })
+
+        const purchaseOrdersWithCustomer = await Promise.all(purchaseOrders.map(async (purchaseOrder) => {
+            purchaseOrder = purchaseOrder.toJSON();
             const user = await User.findOne({ where: { userId: purchaseOrder.customerId } });
             purchaseOrder.customerName = `${user.firstName} ${user.lastName}`
+            return purchaseOrder;
+        }))
 
-        })
-        res.status(200).json(purchaseOrders);
+        res.status(200).json(purchaseOrdersWithCustomer);
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Internal server error" });
