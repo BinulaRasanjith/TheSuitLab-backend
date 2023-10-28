949bb98e257552a7de2d140bfc08be066ad05724
Thimet <92925044+Thimet@users.noreply.github.com>
2023-10-28 23:36:50 +0530

    created addToCard function for standard sizes

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -3,15 +3,15 @@ import { Router } from "express";
 import { authUserRole, authJWT } from "../middlewares/authUser.js";
 // import { ADMIN } from "../constants/constants.js";
 import {
-    setCoatMeasurements,
-    setTrouserMeasurements,
-    getCoatMeasurements,
-    getTrouserMeasurements,
-    setCartItemForCustomSuit,
-    getCartItems,
-    setCartItemForHireCostume,
-    setNewCostumeToItemModel,
-    removeCartItem
+  setCoatMeasurements,
+  setTrouserMeasurements,
+  getCoatMeasurements,
+  getTrouserMeasurements,
+  setCartItemForCustomSuit,
+  getCartItems,
+  setCartItemForHireCostume,
+  setNewCostumeToItemModel,
+  removeCartItem,
 } from "../controllers/customerController.js";
 
 const router = Router();
@@ -24,7 +24,11 @@ router.get("/trouser-measurements/:userId", getTrouserMeasurements);
 router.post("/add-hire-costume-to-cart", authJWT, setCartItemForHireCostume);
 router.post("/add-custom-suit-to-cart", authJWT, setCartItemForCustomSuit);
 router.get("/cart", authJWT, getCartItems);
-router.post("/add-new-costume-to-item-model", authJWT, setNewCostumeToItemModel);
+router.post(
+  "/add-new-costume-to-item-model",
+  authJWT,
+  setNewCostumeToItemModel
+);
 router.delete("/remove-cart-item/:id", authJWT, removeCartItem);
 
 export default router;
--- routes/purchaseOrderRoutes.js
+++ routes/purchaseOrderRoutes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getPurchaseOrders, getPurchaseOrder } from "../controllers/purchaseOrderController.js";
+import {
+  getPurchaseOrders,
+  getPurchaseOrder,
+  getPrice,
+} from "../controllers/purchaseOrderController.js";
 
 const router = Router();
 
 router.get("/", getPurchaseOrders);
 router.get("/:orderId", getPurchaseOrder);
+router.post("/get-price", getPrice);
 
 export default router;
