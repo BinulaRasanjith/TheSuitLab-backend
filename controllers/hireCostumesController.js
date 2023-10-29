70ea8ce61c9303da1221d912c14dcb1a9f81c96a
Thimet <92925044+Thimet@users.noreply.github.com>
2023-10-29 14:30:51 +0530

    created hire costume function

--- controllers/hireCostumesController.js
+++ controllers/hireCostumesController.js
@@ -1,45 +1,45 @@
 import { HireCostume, ItemModel } from "../models/models.js";
 
 export const getHireCostumes = async (req, res) => {
-    try {
-        const { costumeType, rentStatus } = req.query;
-        let hireCostumes;
-        if (costumeType || available) {
-            hireCostumes = await HireCostume.findAll({
-                where: {
-                    costumeType,
-                    rentStatus,
-                },
-            });
-        } else {
-            hireCostumes = await HireCostume.findAll();
-        }
-
-        res.status(200).json(hireCostumes);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Internal server error" });
+  try {
+    const { costumeType, rentStatus } = req.query;
+    let hireCostumes;
+    if (costumeType || available) {
+      hireCostumes = await HireCostume.findAll({
+        where: {
+          costumeType,
+          rentStatus,
+        },
+      });
+    } else {
+      hireCostumes = await HireCostume.findAll();
     }
+
+    res.status(200).json(hireCostumes);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const getHireCostumeById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const hireCostume = await HireCostume.findOne({
-            where: { itemId: id },
-        });
+  try {
+    const { id } = req.params;
+    const hireCostume = await HireCostume.findOne({
+      where: { itemId: id },
+    });
 
-        const itemModel = await ItemModel.findOne({
-            where: { itemId: id },
-        });
+    const itemModel = await ItemModel.findOne({
+      where: { itemId: id },
+    });
 
-        if (hireCostume) {
-            res.status(200).json({ ...hireCostume.toJSON(), ...itemModel.toJSON() });
-        } else {
-            res.status(404).json({ message: "Hire costume not found" });
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Internal server error" });
+    if (hireCostume) {
+      res.status(200).json({ ...hireCostume.toJSON(), ...itemModel.toJSON() });
+    } else {
+      res.status(404).json({ message: "Hire costume not found" });
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,38 +1,36 @@
-import { Router } from 'express'; // for creating router
+import { Router } from "express"; // for creating router
 
-import authRoutes from './authRoutes.js'; // for routing to auth endpoints
-import otpRoutes from './otpRoutes.js'; // for routing to otp endpoints
-import materialRoutes from './materialRoutes.js'; // for routing to material endpoints
-import userRoutes from './userRoutes.js'; // for routing to user endpoints
-import supplierRoutes from './supplierRoutes.js';
-import returnRoutes from './returnRoutes.js';
-import handoverRoutes from './handoverRoutes.js';
-import customerRoutes from './customerRoutes.js';
-import smsRoutes from './smsRoutes.js';
-import dashboardRoutes from './dashboardRoutes.js'; // TODO: IF USING DASHBOARD, UNCOMMENT THIS LINE
-import odbRoutes from './odbRoutes.js'; // TODO: IF USING DASHBOARD, UNCOMMENT THIS LINE
-import hireCostumesRoutes from './hireCostumeRoutes.js';
-import purchaseOrderRoutes from './purchaseOrderRoutes.js';
-import { dashboardData } from '../controllers/dashboardController.js';
-
+import authRoutes from "./authRoutes.js"; // for routing to auth endpoints
+import otpRoutes from "./otpRoutes.js"; // for routing to otp endpoints
+import materialRoutes from "./materialRoutes.js"; // for routing to material endpoints
+import userRoutes from "./userRoutes.js"; // for routing to user endpoints
+import supplierRoutes from "./supplierRoutes.js";
+import returnRoutes from "./returnRoutes.js";
+import handoverRoutes from "./handoverRoutes.js";
+import customerRoutes from "./customerRoutes.js";
+import smsRoutes from "./smsRoutes.js";
+import dashboardRoutes from "./dashboardRoutes.js"; // TODO: IF USING DASHBOARD, UNCOMMENT THIS LINE
+import odbRoutes from "./odbRoutes.js"; // TODO: IF USING DASHBOARD, UNCOMMENT THIS LINE
+import hireCostumesRoutes from "./hireCostumeRoutes.js";
+import purchaseOrderRoutes from "./purchaseOrderRoutes.js";
+import { dashboardData } from "../controllers/dashboardController.js";
 
 const router = Router(); // for creating router
 
-router.use('/auth', authRoutes);
-router.use('/otp', otpRoutes);
-router.use('/user', userRoutes);
-router.use('/material', materialRoutes);
-router.use('/supplier', supplierRoutes);
-router.use('/returns', returnRoutes);
-router.use('/hiring', handoverRoutes);
-router.use('/customer', customerRoutes);
-router.use('/notify', smsRoutes);
-router.use('/dashboard', dashboardRoutes); // TODO: IF USING DASHBOARD, UNCOMMENT THIS LINE
-router.use('/odb', odbRoutes); // TODO: IF USING DASHBOARD, UNCOMMENT THIS LINE
-router.use('/hire-costume', hireCostumesRoutes);
-router.use('/purchase-order', purchaseOrderRoutes);
+router.use("/auth", authRoutes);
+router.use("/otp", otpRoutes);
+router.use("/user", userRoutes);
+router.use("/material", materialRoutes);
+router.use("/supplier", supplierRoutes);
+router.use("/returns", returnRoutes);
+router.use("/hiring", handoverRoutes);
+router.use("/customer", customerRoutes);
+router.use("/notify", smsRoutes);
+router.use("/dashboard", dashboardRoutes); // TODO: IF USING DASHBOARD, UNCOMMENT THIS LINE
+router.use("/odb", odbRoutes); // TODO: IF USING DASHBOARD, UNCOMMENT THIS LINE
+router.use("/costume-hiring", hireCostumesRoutes);
+router.use("/purchase-order", purchaseOrderRoutes);
 
-
-router.get('/assistant', dashboardData);
+router.get("/assistant", dashboardData);
 
 export default router;
