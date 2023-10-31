43c29831a2d0db15d41c419e92214bf819fdd728
Affadha1967 <130926229+Affadha1967@users.noreply.github.com>
2023-10-31 15:18:19 +0530

    Update supplierRoutes.js

--- routes/supplierRoutes.js
+++ routes/supplierRoutes.js
@@ -4,15 +4,16 @@ import {
     createSupplier,
     deleteSupplier,
     getSupplier,
-    getSuppliers, updateSupplier,
+    getSuppliers,
+    updateSupplier,
 } from "../controllers/supplierController.js";
 
 const router = Router();
 
-router.post("/create", createSupplier);
-router.get("/get", getSuppliers);
-router.get("/get/:id", getSupplier);
-router.patch("/update", updateSupplier);
-router.delete("/delete/:id", deleteSupplier);
+router.get("/", getSuppliers);
+router.get("/:id", getSupplier);
+router.post("/", createSupplier);
+router.patch("/", updateSupplier);
+router.delete("/:id", deleteSupplier);
 
 export default router;
