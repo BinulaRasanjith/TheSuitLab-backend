715a26552d1e224f24213a4569814d7aebe7ed7c
Thimet <92925044+Thimet@users.noreply.github.com>
2023-11-01 03:37:21 +0530

    added data into belt seed

--- routes/accessoryRoutes.js
+++ routes/accessoryRoutes.js
@@ -1,20 +1,22 @@
 import { Router } from "express";
 
 import {
-    addNewAccessory,
-    getAccessories,
-    getAccessory,
-    removeAccessory,
-    updateAccessory,
+  addNewAccessory,
+  getAccessories,
+  getAccessoryById,
+  removeAccessory,
+  updateAccessory,
+  addAccessoryToCart,
 } from "../controllers/accessoryController.js";
 import { accessoryImageUpload } from "../middlewares/imageUpload.js";
 
 const router = Router();
 
 router.get("/", getAccessories);
-router.get("/:id", getAccessory);
+router.get("/:type/:id", getAccessoryById);
 router.post("/add", accessoryImageUpload.array("images", 10), addNewAccessory);
 router.post("/remove", removeAccessory);
 router.post("/update", updateAccessory);
+router.post("/add-accessory-to-cart", addAccessoryToCart);
 
 export default router;
