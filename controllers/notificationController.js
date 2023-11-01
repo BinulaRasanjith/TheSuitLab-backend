1daca0d7f7b6a572173b27800680fc411c122d2d
Binula Rasanjith <binula28@gmail.com>
2023-11-01 06:29:08 +0530

    set notifications to read method

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -14,3 +14,18 @@ export const getNotifications = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 };
+
+export const setNotificationsToRead = async (req, res) => {
+    const { userId } = req.user;
+
+    try {
+        await Notification.update(
+            { isRead: true },
+            { where: { userId, isRead: false } }
+        );
+        res.status(200).json({ message: "Notifications set to read" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
