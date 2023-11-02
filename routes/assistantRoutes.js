649e9e7260bd300619295774bb5ca843f18e0fde
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-11-02 10:49:05 +0530

    dashboard update

--- routes/assistantRoutes.js
+++ routes/assistantRoutes.js
@@ -4,12 +4,13 @@ import { Router } from "express";
 // import { OPERATION_ASSISTANT, PRODUCT_MANAGER, TAILOR, ADMIN } from "../constants/constants.js";
 
 import { dashboardData } from "../controllers/dashboardController.js";
-import { removeItem } from "../controllers/assistantController.js";
+import { removeItem, recentOrders } from "../controllers/assistantController.js";
 
 const router = Router();
 // const allowedRoles = [ OPERATION_ASSISTANT ];
 
 router.get("/", dashboardData); // ! ASSISTANT DASHBOARD RUNS USING THIS ENDPOINT
+router.get("/recent-orders", recentOrders);
 router.delete("/remove-item", removeItem);
 
 export default router;
